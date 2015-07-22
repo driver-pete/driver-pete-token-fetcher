42041d0fbe70a0cf681e76b342ba0f6bc6bce53b
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-07-21 21:35:36 -0700

    single jar and parsing

--- src/main/java/com/otognan/driverpete/token_fetcher/TokenFetcher.java
+++ src/main/java/com/otognan/driverpete/token_fetcher/TokenFetcher.java
@@ -1,9 +1,24 @@
 package com.otognan.driverpete.token_fetcher;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 public class TokenFetcher {
 
-    public static void main(String[] args) {
-        System.out.println("HELLO");
+    public static void main(String[] args) throws ParseException {
+        
+        Options options = new Options();
+        options.addOption("t", true, "display current time");
+        
+        CommandLineParser parser = new DefaultParser();
+        
+        CommandLine cmd = parser.parse( options, args);
+        
+        String countryCode = cmd.getOptionValue("t");
+        System.out.println(countryCode);
     }
 
 }
