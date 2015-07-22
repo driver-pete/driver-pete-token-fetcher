41b1bccf02e289e64c9fb351466a55470389f5f2
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-07-21 22:27:31 -0700

    working thing

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # driver-pete-token-fetcher
 Fetches authorization token from the server
+
+To build:
+./gradlew fatJar
+
+Artefact driver-pete-token-fetcher.jar will be in build/libs
+
+usage: driver-pete-token-fetcher
+ -address <arg>   Server address (e.g. https://localhost:8443)
+ -email <arg>     Facebook email (e.g. testuser@gmail.com)
+ -pass <arg>      Facebook password
+
+Example:
+java -jar driver-pete-token-fetcher.jar -address https://localhost:8443 -email <FACEBOOK_EMAIL> -pass <FACEBOOK_PASSWORD>
--- src/main/java/com/otognan/driverpete/token_fetcher/TokenFetcher.java
+++ src/main/java/com/otognan/driverpete/token_fetcher/TokenFetcher.java
@@ -1,24 +1,97 @@
 package com.otognan.driverpete.token_fetcher;
 
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.logging.Level;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import com.gargoylesoftware.htmlunit.util.Cookie;
+
 public class TokenFetcher {
 
-    public static void main(String[] args) throws ParseException {
+    public static void main(String[] args) throws Exception {
+        
+        java.util.logging.Logger.getLogger("com.gargoylesoftware.htmlunit").setLevel(Level.OFF);
+        java.util.logging.Logger.getLogger("org.apache.http.client.protocol.ResponseProcessCookies").setLevel(Level.OFF);
         
         Options options = new Options();
-        options.addOption("t", true, "display current time");
+        options.addOption("address", true, "Server address (e.g. https://localhost:8443)");
+        options.addOption("email", true, "Facebook email (e.g. testuser@gmail.com)");
+        options.addOption("pass", true, "Facebook password");
         
         CommandLineParser parser = new DefaultParser();
+ 
+        try {
+            // parse the command line arguments
+            CommandLine cmd = parser.parse( options, args);
+            
+            String address = cmd.getOptionValue("address");
+            String email = cmd.getOptionValue("email");
+            String pass = cmd.getOptionValue("pass");
+            
+            if (address == null) {
+                throw new ParseException("Server adrress can not be null");
+            }
+            
+            if (email == null) {
+                throw new ParseException("Email can not be null");
+            }
+            
+            if (pass == null) {
+                throw new ParseException("Password can not be null");
+            }
+
+            String token = TokenFetcher.fetch_token(address, email, pass);
+            System.out.println(token);
+        }
+        catch( ParseException exp ) {
+            System.out.println("Unexpected exception: " + exp.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("driver-pete-token-fetcher", options );
+            System.exit(-1);
+        }
+    }
+    
+    public static String fetch_token(String serverAddress, String facebookUsername, String facebookPassword) throws Exception {
+        // Perform facebook login automation with HTMLUnit
+        WebClient webClient = new WebClient();
+        // Disable SSL - otherwise redirect from facebook to our app will fail
+        // because of testing certificates
+        webClient.getOptions().setUseInsecureSSL(true);
         
-        CommandLine cmd = parser.parse( options, args);
-        
-        String countryCode = cmd.getOptionValue("t");
-        System.out.println(countryCode);
+        HtmlPage page1 = webClient.getPage(serverAddress + "/auth/facebook");
+        HtmlForm form = (HtmlForm) page1.getElementById("login_form");
+        if (form == null) {
+            throw new Exception("Login form is not found. Possibly we are not on login page. " +
+                    "Most of the time it happens when facebook rejects our app url");
+        }
+        HtmlSubmitInput button = (HtmlSubmitInput) form.getInputsByValue("Log In").get(0);
+        HtmlTextInput textField = form.getInputByName("email");
+        textField.setValueAttribute(facebookUsername);
+        HtmlPasswordInput textField2 = form.getInputByName("pass");
+        textField2.setValueAttribute(facebookPassword);
+
+        HtmlPage homePage = button.click();
+
+        // Check that we are redirected back to the application
+        Cookie tokenCookie = webClient.getCookieManager().getCookie("AUTH-TOKEN");
+        String token = tokenCookie.getValue();
+        return token;
     }
 
 }
